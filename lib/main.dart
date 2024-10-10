86139de6038313e921ec3f4f650a6117d0b07b4f
Kemo Emam <koko.bose85@gmail.com>
2024-10-10 21:50:41 +0300

    -fake push to origin development branch

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:medi_care_hub/core/router/app_router.dart';
 import 'package:medi_care_hub/medi_care.dart';
 
 void main() {
+  //
   runApp(
     MediCare(appRouter: AppRouter()),
   );
