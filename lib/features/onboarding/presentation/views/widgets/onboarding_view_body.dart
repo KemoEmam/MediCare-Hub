ac609078dab455ef61a23100176fed76a9d59e17
Kemo Emam <koko.bose85@gmail.com>
2024-10-13 23:09:48 +0300

    Refactor OnBoarding

--- lib/features/onboarding/presentation/views/widgets/onboarding_view_body.dart
+++ lib/features/onboarding/presentation/views/widgets/onboarding_view_body.dart
@@ -9,18 +9,6 @@ class OnBoardingViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
-    return Padding(
-      padding: EdgeInsets.only(top: 30.h),
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            const DocLogoAndName(),
-            SizedBox(height: 40.h),
-            const DoctorImageAndText(),
-            SizedBox(height: 18.h),
-            const GetStartedAndDescription(),
-=======
     return SingleChildScrollView(
       child: Padding(
         padding: EdgeInsets.symmetric(horizontal: 6.w, vertical: 25.h),
@@ -32,7 +20,6 @@ class OnBoardingViewBody extends StatelessWidget {
             SizedBox(height: 18.h),
             const GetStartedAndDescription(),
             // SizedBox(height: 30.h),
->>>>>>> e3ec78a4f6a7acdb305b62e3c1513cb5eca8fa16
           ],
         ),
       ),
